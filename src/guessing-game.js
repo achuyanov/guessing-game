930739471e9a0e9458896874609c6a418bfb0af8
AndreiChuyanov <achuyanov@gmail.com>
2019-10-06 11:09:15 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
+
+    //this method accepts min and max value of range of number to guess
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+
+    //this method returns solution candidate (you make an assumption based on range and previous assumptions)
     guess() {
-
+        this.mid = Math.ceil((this.max + this.min) / 2);
+        return this.mid;
     }
 
+
+    //this method is called if prev call of `guess()` returned number which is greater than answer
     lower() {
+        this.max = this.mid;
 
     }
 
-    greater() {
 
+    //this method is called if prev call of `guess()` returned number which is lower than answer
+    greater() {
+        this.min = this.mid;
     }
 }
 
